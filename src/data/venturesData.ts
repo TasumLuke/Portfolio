47966c939187945242f1a0f8c2c0227666473645
Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>
2025-03-21 15:27:17 -0400

    Update venturesData.ts

--- src/data/venturesData.ts
+++ src/data/venturesData.ts
@@ -7,6 +7,7 @@ interface Venture {
   technologies: string[];
   imageUrl: string;
   year: number;
+  websiteUrl?: string;
 }
 
 export const ventures: Venture[] = [
@@ -18,6 +19,7 @@ export const ventures: Venture[] = [
     technologies: ["Linguistics", "Research", "Cultural Preservation"],
     imageUrl: "https://static.wixstatic.com/media/f961f7_b1713a0d871c420cbaf091ac1dc9fd01~mv2.jpeg/v1/fill/w_850,h_729,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/f961f7_b1713a0d871c420cbaf091ac1dc9fd01~mv2.jpeg",
     year: 2024
+    websiteUrl: "https://taniyuzin.wixsite.com/tani-foundation/about-2"
   },
   {
     id: 2,
@@ -27,6 +29,7 @@ export const ventures: Venture[] = [
     technologies: ["Mentorship", "Research"],
     imageUrl: "https://mountainecho.in/wp-content/uploads/2024/03/NECHR-Cultivating-research-pioneers-in-Northeast-Indias-High-Schools-1024x678.webp",
     year: 2023
+    websiteUrl: "https://nechr-research.us/"
   },
   {
     id: 3,
@@ -36,6 +39,7 @@ export const ventures: Venture[] = [
     technologies: ["River Conservation", "Research"],
     imageUrl: "https://static.wixstatic.com/media/f1ea87_573e1d10e76949548959cb53e0f5f26d~mv2.png/v1/fill/w_236,h_233,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/f1ea87_573e1d10e76949548959cb53e0f5f26d~mv2.png",
     year: 2024
+    websiteUrl: "https://ngoksiang.org/"
   },
   
 ];
