2dee70022a7f59d0d208eb71f98772a7c4f1ae2c
Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>
2025-03-21 15:14:40 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,6 @@ const App = () => {
               <Route path="/ventures" element={<Ventures />} />
               <Route path="/news" element={<News />} />
               <Route path="/contact" element={<Contact />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </AnimatePresence>
